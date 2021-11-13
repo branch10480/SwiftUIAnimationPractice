b94575c5924df573e9f4044abafe182b104138a2
branch10480 <toshiharu.imaeda@gmail.com>
2021-11-13 17:54:14 +0900

    Add

--- SwiftUIAnimationPractice/ContentView.swift
+++ SwiftUIAnimationPractice/ContentView.swift
@@ -24,18 +24,20 @@ struct MainView: View {
   
   var body: some View {
     Button(action: {
-      // Stateの変更部分（.toggle()） を withAnimation でくくると、そのStateに関連するすべてのビューがアニメーションする
-      // withAnimation(.spring()) { ... で .sprint() アニメーション指定ができる
       withAnimation(.spring()) {
-        self.showDetail.toggle()
+        showDetail.toggle()
       }
     }) {
-      Image(systemName: "chevron.right.circle")
-        .font(.system(size: 50))
-        .rotationEffect(.degrees(showDetail ? 90 : 0))
-        // withAnimation を上書きできる（この場合、rotationEffectはアニメーションしない）
-        .animation(nil, value: showDetail)
-        .scaleEffect(showDetail ? 1.5 : 1)
+      VStack {
+        Image(systemName: "chevron.right.circle").font(.system(size: 50))
+          .rotationEffect(.degrees(showDetail ? 90 : 0))
+        
+        if showDetail {
+          Text("Detail")
+        }
+        
+        Spacer()
+      }
     }
   }
 }
@@ -56,6 +58,27 @@ struct Step1View: View {
       // .speed() で元々のアニメーション速度を乗算で変化させられる
       // .speed(0.5) は速度を0.5倍することなので2倍遅くなる
         .animation(.spring().speed(0.5), value: showDetail)
+    }
+  }
+}
+
+struct Step2View: View {
+  @State var showDetail: Bool
+  
+  var body: some View {
+    Button(action: {
+      // Stateの変更部分（.toggle()） を withAnimation でくくると、そのStateに関連するすべてのビューがアニメーションする
+      // withAnimation(.spring()) { ... で .sprint() アニメーション指定ができる
+      withAnimation(.spring()) {
+        self.showDetail.toggle()
+      }
+    }) {
+      Image(systemName: "chevron.right.circle")
+        .font(.system(size: 50))
+        .rotationEffect(.degrees(showDetail ? 90 : 0))
+      // withAnimation を上書きできる（この場合、rotationEffectはアニメーションしない）
+        .animation(nil, value: showDetail)
+        .scaleEffect(showDetail ? 1.5 : 1)
     }
   }
 }
